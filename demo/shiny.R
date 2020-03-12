e81c3447c92b035691f24fa13d74cd9c179cb04c
ampatspell <ampatspell@gmail.com>
2020-03-13 00:57:44 +0200

    update shiny demo

--- demo/shiny.R
+++ demo/shiny.R
@@ -1,5 +1,9 @@
 library(shiny)
 library(tradingview)
+library(tidyverse)
+library(tidyquant)
+
+aapl <- tq_get("AAPL") %>% mutate(time = date)
 
 ui = shinyUI(fluidPage(
   tradingviewOutput("tv")
@@ -7,7 +11,28 @@ ui = shinyUI(fluidPage(
 
 server = function(input, output) {
   output$tv <- renderTradingview({
-    tradingview(list())
+    tradingview(aapl, list(
+      chart = list(
+        decimals = 2,
+        margins = list(
+          top = 0,
+          bottom = 0.2
+        )
+      ),
+      candles = list(
+        type = "candles"
+      ),
+      volume = list(
+        type = "histogram",
+        value = "volume",
+        color = "rgba(0,0,0,0.2)",
+        overlay = TRUE,
+        margins = list(
+          top = 0.8,
+          bottom = 0
+        )
+      )
+    ), height = 400)
   })
 }
 
